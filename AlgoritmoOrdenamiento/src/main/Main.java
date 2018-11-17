19b027479ad4f834822ded77b5191d9f026a2efc
Ricardo Serrano <rxsh964@gmail.com>
2018-11-17 17:21:09 -0500

    Metodos para Escritura y Lectura de Archivos

--- AlgoritmoOrdenamiento/src/main/Main.java
+++ AlgoritmoOrdenamiento/src/main/Main.java
@@ -6,6 +6,8 @@
 package main;
 
 import java.util.Random;
+import model.NumberGenerator;
+import model.io.IOFile;
 import model.tda.ArrayList;
 
 /**
@@ -37,7 +39,7 @@ public class Main {
         System.out.println("");
         System.out.println("============================================");
         System.out.println("Creando ArrayList MERGE");
-        ArrayList<Integer>  merge = new ArrayList<Integer>();
+        ArrayList<Integer>  merge = new ArrayList<>();
         System.out.println(merge);
         System.out.println("Ingresando Elementos Aleatorios al ArrayList MERGE");
         for (int i = 0; i < 20; i++) {
@@ -76,6 +78,28 @@ public class Main {
         stooge.stoogeSort();
         System.out.println(stooge);
         System.out.println("============================================");
+        System.out.println("PROBANDO EL RANDOM");
+        
+        System.out.println("PROBANDO EL METODO PARA LA CREACION Y ESCRITURA DE FILES");
+        NumberGenerator ng1 = new NumberGenerator(); //ALEATORIOS ENTRE 0 - 100 -> 10 ELEMENTOS
+        NumberGenerator ng2 = new NumberGenerator(100,200); //ALEATORIOS ENTRE 100 - 200 -> 10 ELEMENTOS
+        NumberGenerator ng3 = new NumberGenerator(100,200,20);//ALEATORIOS ENTRE 100 - 200 -> 20 ELEMENTOS
+        IOFile.generateFile("prueba1.txt", ng1);
+        IOFile.generateFile("prueba2.txt", ng2);
+        IOFile.generateFile("prueba3.txt", ng3);
+        
+        System.out.println("PROBANDO EL METODO PARA LA LECTURA DEL ARCHIVO");
+        ArrayList<Integer> array1 = IOFile.readFile("prueba1.txt"); //GUARDO TODAS LAS LINEAS DEL ARCHIVO EN EL ARRAY
+        ArrayList<Integer> array2 = IOFile.readFile("prueba2.txt", 5); //GUARDO LAS PRIMERAS 5 LINEAS DEL ARCHIVO EN EL ARRAY
+        ArrayList<Integer> array3 = IOFile.readFile("prueba3.txt", 14); //GUARDO LAS PRIMERAS 14 LINEAS DEL ARCHIVO EN EL ARRAY
+        ArrayList<Integer> array4 = IOFile.readFile("prueba3.txt", 15); //GUARDO LAS PRIMERAS 15 ...
+        ArrayList<Integer> array5 = IOFile.readFile("prueba3.txt", 60); //COMO 60 > A LA CANTIDAD DE ELEMENTOS DEL ARRAY, SIMPLEMENTE SE GUARDAN TODOS LOS ELEMENTOS DEL ARCHIVO AL ARRAY
+        
+        System.out.println(array1);
+        System.out.println(array2);
+        System.out.println(array3);
+        System.out.println(array4);
+        System.out.println(array5);
         
     }
 }
---
+++ AlgoritmoOrdenamiento/src/model/io/IOFile.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.io;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.NumberGenerator;
+import model.tda.ArrayList;
+
+/**
+ *
+ * @author rxsh96
+ */
+public class IOFile {
+    
+    private final static String FILEPATH = "files/";
+    
+    /**
+     * Método que genera un archivo con números aleatorios línea por línea. 
+     * Si el archivo no existe, crea uno y almacena en él números aleatorios.
+     * En caso de que el archivo exista, concatena al final del archivo más números aleatorios.
+     * @param filename, nombre del archivo.
+     * @param ng, objeto NumberGenerator.
+     */
+    public static void generateFile(String filename, NumberGenerator ng){
+        File file = new File(FILEPATH+filename);
+        try {
+            FileWriter writer;
+            if (file.exists()){
+                writer = new FileWriter(file, true);
+            } 
+            else{
+                writer = new FileWriter(file);
+            }
+            try(PrintWriter printW = new PrintWriter(writer)){
+                for (int i = 0; i < ng.getAmount(); i++) {
+                    printW.println(ng.generateNumber());
+                }
+                writer.close();
+            }
+        } 
+        catch(IOException ex){
+            Logger.getLogger(IOFile.class.getName()).log(Level.SEVERE, null, ex);
+        }    
+    }
+    
+    /**
+     * Método que lee las líneas de un archivo y almacena el contenido en un ArrayList.
+     * @param filename, nombre del archivo.
+     * @param amount, cantidad de lineas que se va a leer. 
+     * @return ArrayList de elementos del archivo.
+     */
+    public static ArrayList<Integer> readFile(String filename, int amount){
+        File file = new File(FILEPATH+filename);
+        ArrayList<Integer> array = null;
+        if (!file.exists()) {
+            System.out.println("FILE NOT FOUND");
+        }
+        int fileLines = fileLines(file);
+        if(fileLines < amount){
+            amount = fileLines;
+        }
+        try (Scanner lector = new Scanner(file)){
+            array = new ArrayList<>();
+            for (int i = 0; i < amount; i++) {
+                array.addFirst(Integer.parseInt(lector.nextLine()));
+            }
+            lector.close();
+        } 
+        catch (FileNotFoundException ex){
+            Logger.getLogger(IOFile.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return array;
+    }
+    
+    /**
+     * Método que lee las líneas del archivo y almacena el contenido en un ArrayList.
+     * @param filename, nombre del archivo.
+     * @return ArrayList de elementos del archivo.
+     */
+    public static ArrayList<Integer> readFile(String filename){
+        File file = new File(FILEPATH+filename);
+        ArrayList<Integer> array = null;
+        if (!file.exists()) {
+            System.out.println("El archivo no existe");
+        }
+        try (Scanner lector = new Scanner(file)){
+            array = new ArrayList<>();
+            while(lector.hasNextLine()){
+                array.addFirst(Integer.parseInt(lector.nextLine()));
+            }
+            lector.close();
+        } 
+        catch (FileNotFoundException ex){
+            Logger.getLogger(IOFile.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return array;
+    }
+    
+    /**
+     * Determina la cantidad de líneas de un archivo
+     * @param file, descriptor del archivo.
+     * @return cantidad de líneas del archivo.
+     */
+    public static int fileLines(File file){
+        int lineN = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            while (reader.readLine() != null){
+                lineN++;
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(IOFile.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return lineN;
+    }
+}
